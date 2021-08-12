35100a523824f2130b12659777cebf3e7a714a79
Kardelen Yalnız <kardelen.yalniz@tcm.com.tr>
2021-08-12 14:07:57 +0300

    Add project files.

---
+++ Core/Utilities/Results/ErrorResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class ErrorResult: Result 
+    { 
+        public ErrorResult(bool success, string message) : base(success:false, message)
+        {
+
+        }
+
+        public ErrorResult(bool success  ) : base(success:false )
+        {
+
+        }
+
+    }
+}
